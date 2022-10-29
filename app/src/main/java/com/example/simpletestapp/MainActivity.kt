abde3f8878108ababda7a048ebca1c8b232b7bf3
faiz baraja <faiz.baraja89@gmail.com>
2022-10-29 15:56:51 +0700

    feat : created page for ibtisam faiz baraja

--- app/src/main/java/com/example/simpletestapp/MainActivity.kt
+++ app/src/main/java/com/example/simpletestapp/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.simpletestapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonIbtisam = findViewById(R.id.ibtisamButton) as Button
+        buttonIbtisam.setOnClickListener {
+            val intent = Intent(this, IbtisamBarajaActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
